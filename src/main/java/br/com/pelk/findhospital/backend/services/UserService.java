4ce754bc853b5d4f91a67fa8b3aad0cd3925c196
KIRK SAHDO <ksahdo10@gmail.com>
2022-09-23 23:11:35 -0400

    feat: add user service class

---
+++ src/main/java/br/com/pelk/findhospital/backend/services/UserService.java
@@ -0,0 +1,9 @@
+package br.com.pelk.findhospital.backend.services;
+
+import br.com.pelk.findhospital.models.User;
+
+public final class UserService {
+    public static boolean createUser(User user) {
+        return false;
+    }
+}
---
+++ src/main/java/br/com/pelk/findhospital/models/User.java
@@ -0,0 +1,119 @@
+package br.com.pelk.findhospital.models;
+
+public class User {
+    private String name;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+    private String rg;
+    private String cpf;
+    private String adress;
+    private String cep;
+    private String phoneNumber;
+    private boolean location;
+
+    public User(String name, String lastName, String email, String username, String password, String rg, String cpf, String adress, String cep, String phoneNumber, boolean location) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.rg = rg;
+        this.cpf = cpf;
+        this.adress = adress;
+        this.cep = cep;
+        this.phoneNumber = phoneNumber;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isLocation() {
+        return location;
+    }
+
+    public void setLocation(boolean location) {
+        this.location = location;
+    }
+    
+    
+}

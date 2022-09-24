0ecd1b822d48b3e54d8f60553b579547fff6654d
KIRK SAHDO <ksahdo10@gmail.com>
2022-09-23 22:56:45 -0400

    feat: add files

---
+++ src/main/java/br/com/pelk/findhospital/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.pelk.findhospital;
+
+import br.com.pelk.findhospital.frames.NotLoggedFrame;
+
+/**
+ *
+ * @author Ayato
+ */
+public class Main {
+    public static void main(String[] args) {
+        NotLoggedFrame frame = new NotLoggedFrame();
+        frame.setVisible(true);
+    }
+}

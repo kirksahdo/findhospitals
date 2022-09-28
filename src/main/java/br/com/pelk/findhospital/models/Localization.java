16609bde8be3aabd05c8b8f8a766abd2da2ff917
Eric <ericd.perin@gmail.com>
2022-09-28 17:01:21 -0400

    feat: add calss Localization

---
+++ src/main/java/br/com/pelk/findhospital/models/Localization.java
@@ -0,0 +1,67 @@
+
+package br.com.pelk.findhospital.models;
+
+import java.io.Serializable;
+
+public class Localization implements Serializable{
+    private String road;
+    private String district;
+    private String number;
+    private String cep;
+    private String complement;
+
+    public Localization(String road, String district, String number, String cep, String complement) {
+        this.road = road;
+        this.district = district;
+        this.number = number;
+        this.cep = cep;
+        this.complement = complement;
+    }
+
+    public String getRoad() {
+        return road;
+    }
+
+    public void setRoad(String road) {
+        this.road = road;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+    public String getLocalization(){
+        return this.road;
+        
+    }
+    
+    
+    
+}

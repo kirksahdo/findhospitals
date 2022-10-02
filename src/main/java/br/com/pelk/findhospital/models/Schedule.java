2a3d6e5c1a605f47348d3805586b101a236936b0
Alien <Alien@DESKTOP-0V1E1AT>
2022-10-02 01:35:30 -0400

    feat: add class Schedule

---
+++ src/main/java/br/com/pelk/findhospital/models/Schedule.java
@@ -0,0 +1,23 @@
+package br.com.pelk.findhospital.models;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Schedule {
+	private ArrayList<Appointment> appointments = new ArrayList<Appointment>();
+	
+	public Schedule(){
+		this.appointments = new ArrayList<Appointment>();
+	}
+	
+	public void addAppointment(Appointment appointment) {
+	
+	}
+	
+	public void removeAppointment(Appointment appointment) {
+		
+	}
+	
+	public void consultAppointment() {
+		
+	}
+}

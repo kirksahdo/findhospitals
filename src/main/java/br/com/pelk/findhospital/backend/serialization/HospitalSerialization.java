482a6831a934558ec28b0c2739fe3e3605194eba
KIRK SAHDO <ksahdo10@gmail.com>
2022-10-08 15:02:29 -0400

    feat: add hospital serialization

--- src/main/java/br/com/pelk/findhospital/backend/serialization/HospitalSerialization.java
+++ src/main/java/br/com/pelk/findhospital/backend/serialization/HospitalSerialization.java
@@ -1,6 +1,6 @@
 package br.com.pelk.findhospital.backend.serialization;
 
-import br.com.pelk.findhospital.models.User;
+import br.com.pelk.findhospital.models.Hospital;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -11,63 +11,63 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public final class UserSerialization {
+public final class HospitalSerialization {
 
     
     private static final String PATH_FILE = "res/";
-    private static final String FILE_NAME = PATH_FILE + "users.txt";
+    private static final String FILE_NAME = PATH_FILE + "hospitals.txt";
     
-    public static ArrayList<User> readFile() {
-        ArrayList<User> users = new ArrayList<>();
+    public static ArrayList<Hospital> readFile() {
+        ArrayList<Hospital> hospitals = new ArrayList<>();
         try {
-            FileInputStream fileInputStream = new FileInputStream(FILE_NAME);
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            User user;
-            Object object;
-            object = objectInputStream.readObject();
-            
-            if(object != null) users = (ArrayList<User>) object;
-            
-            fileInputStream.close();
+            ObjectInputStream objectInputStream;
+            try (FileInputStream fileInputStream = new FileInputStream(FILE_NAME)) {
+                objectInputStream = new ObjectInputStream(fileInputStream);
+                Object object;
+                object = objectInputStream.readObject();
+                if(object != null) hospitals = (ArrayList<Hospital>) object;
+            }
             objectInputStream.close();
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(UserSerialization.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(UserSerialization.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(UserSerialization.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(HospitalSerialization.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(HospitalSerialization.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return users;
+        return hospitals;
     }
     
-    public static void writeFile(User user) {
-        ArrayList<User> users = readFile();
+    public static void writeFile(Hospital hospital) {
+        ArrayList<Hospital> hospitals = readFile();
+        if (hospitals == null) {
+            hospitals = new ArrayList<>();
+        }
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME);
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            
-            users.add(user);
-            objectOutputStream.writeObject(users);
-            fileOutputStream.close();
+            ObjectOutputStream objectOutputStream;
+            try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
+                objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                hospitals.add(hospital);
+                objectOutputStream.writeObject(hospitals);
+            }
             objectOutputStream.close();
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(UserSerialization.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(HospitalSerialization.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
-            Logger.getLogger(UserSerialization.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(HospitalSerialization.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public static void writeFile(ArrayList<User> users) {
+    public static void writeFile(ArrayList<Hospital> hospitals) {
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME);
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            objectOutputStream.writeObject(users);
-            fileOutputStream.close();
+            ObjectOutputStream objectOutputStream;
+            try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
+                objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                objectOutputStream.writeObject(hospitals);
+            }
             objectOutputStream.close();
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(UserSerialization.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(HospitalSerialization.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
-            Logger.getLogger(UserSerialization.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(HospitalSerialization.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
